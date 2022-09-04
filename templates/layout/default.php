666b91d08cd2f37434d6beb6e40fb3392e540375
AllEN <qr1403525394@outlook.com>
2022-09-04 22:06:59 +1000

    fix bug: profile picture

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -179,7 +179,7 @@
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                            <? = $this->Html->image('undraw_profile.svg', ['class' => 'img-profile rounded-circle']); ?>
+                            <?=$this->Html->image('undraw_profile.svg', ['class' => 'img-profile rounded-circle']) ?>
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
