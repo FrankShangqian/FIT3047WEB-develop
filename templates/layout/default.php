a69a2b20fb0ef4ee8f219ff988d7d712a0674f6a
AllEN <qr1403525394@outlook.com>
2022-09-04 16:39:06 +1000

    modify side bar function

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -42,9 +42,9 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="<?= $this->Url->build(['controller'=> 'Pages','action'=>'display','home']) ?>">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+                <span>Home</span></a>
         </li>
 
         <!-- Divider -->
@@ -52,85 +52,74 @@
 
         <!-- Heading -->
         <div class="sidebar-heading">
-            Interface
+            Functions
         </div>
 
-        <!-- Nav Item - Pages Collapse Menu -->
+        <!-- Nav Item Orders -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-               aria-expanded="true" aria-controls="collapseTwo">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOrders"
+               aria-expanded="true" aria-controls="collapseOrders">
                 <i class="fas fa-fw fa-cog"></i>
-                <span>Components</span>
+                <span>Orders</span>
             </a>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div id="collapseOrders" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Components:</h6>
-                    <a class="collapse-item" href="buttons.html">Buttons</a>
-                    <a class="collapse-item" href="cards.html">Cards</a>
+                    <h6 class="collapse-header">Orders functions:</h6>
+                    <a class="collapse-item" href="<?= $this->Url->build('/orders') ?>">List</a>
+                    <a class="collapse-item" href="<?= $this->Url->build('/orders/add') ?>">New Orders</a>
                 </div>
             </div>
         </li>
 
-        <!-- Nav Item - Utilities Collapse Menu -->
+        <!-- Nav Item Customers -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-               aria-expanded="true" aria-controls="collapseUtilities">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCustomers"
+               aria-expanded="true" aria-controls="collapseCustomers">
                 <i class="fas fa-fw fa-wrench"></i>
-                <span>Utilities</span>
+                <span>Customers</span>
             </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            <div id="collapseCustomers" class="collapse" aria-labelledby="headingUtilities"
                  data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Utilities:</h6>
-                    <a class="collapse-item" href="utilities-color.html">Colors</a>
-                    <a class="collapse-item" href="utilities-border.html">Borders</a>
-                    <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                    <a class="collapse-item" href="utilities-other.html">Other</a>
+                    <h6 class="collapse-header">Customers functions:</h6>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Customers') ?>">List</a>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Customers/add') ?>">New Customers</a>
                 </div>
             </div>
         </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Addons
-        </div>
-
-        <!-- Nav Item - Pages Collapse Menu -->
+        <!-- Nav Item Invoices -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-               aria-expanded="true" aria-controls="collapsePages">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Pages</span>
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseInvoices"
+               aria-expanded="true" aria-controls="collapseInvoices">
+                <i class="fas fa-fw fa-wrench"></i>
+                <span>Invoices</span>
             </a>
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div id="collapseInvoices" class="collapse" aria-labelledby="headingUtilities"
+                 data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Login Screens:</h6>
-                    <a class="collapse-item" href="login.html">Login</a>
-                    <a class="collapse-item" href="register.html">Register</a>
-                    <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                    <div class="collapse-divider"></div>
-                    <h6 class="collapse-header">Other Pages:</h6>
-                    <a class="collapse-item" href="404.html">404 Page</a>
-                    <a class="collapse-item" href="blank.html">Blank Page</a>
+                    <h6 class="collapse-header">Invoices function:</h6>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Invoices') ?>">List</a>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Invoices/add') ?>">New Invoices</a>
                 </div>
             </div>
         </li>
 
-        <!-- Nav Item - Charts -->
+        <!-- Nav Item Products -->
         <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Charts</span></a>
-        </li>
-
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Tables</span></a>
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProducts"
+               aria-expanded="true" aria-controls="collapseProducts">
+                <i class="fas fa-fw fa-wrench"></i>
+                <span>Products</span>
+            </a>
+            <div id="collapseProducts" class="collapse" aria-labelledby="headingUtilities"
+                 data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Products function:</h6>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Products') ?>">List</a>
+                    <a class="collapse-item" href="<?= $this->Url->build('/Products/add') ?>">New Products</a>
+                </div>
+            </div>
         </li>
 
         <!-- Divider -->
