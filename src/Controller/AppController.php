8054353b6303e27db56de424a21ae6b9c583e032
Shangqian <sduu0007@student.monash.edu>
2022-10-05 16:26:53 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	templates/Products/add.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -58,6 +58,6 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // for all controllers in our application, make index and view
         // actions public, skipping the authentication check
-        $this->Authentication->addUnauthenticatedActions(['login','display','view','add','orders']);
+        $this->Authentication->addUnauthenticatedActions(['login','index','delete','display','view','add']);
     }
 }
